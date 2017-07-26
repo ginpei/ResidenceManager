89c6a3689973b411fae4f10fc8ba3801b31890b2
Masato Miyazawa <masatomiyazawa1014@gmail.com>
2017-07-26 14:06:56 -0700

    add welcome pages

---
+++ ResidenceManager/Page.swift
@@ -0,0 +1,15 @@
+//
+//  Page.swift
+//  ResidenceManager
+//
+//  Created by Masato Miyazawa on 2017-07-26.
+//  Copyright © 2017 Ginpei. All rights reserved.
+//
+
+import Foundation
+
+struct Page {
+    let title: String
+    let message: String
+    let imageName: String
+}
