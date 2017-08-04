458e78fa82c03a423ff783c945ddace074a9c09d
ginpei <ginpei@gmail.com>
2017-08-03 17:11:59 -0700

    some dynamic UIs

--- ResidenceManager/EventEditViewController.swift
+++ ResidenceManager/EventEditViewController.swift
@@ -10,8 +10,20 @@ import UIKit
 
 class EventEditViewController: UITableViewController {
     @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var alldaySwitch: UISwitch!
+    @IBOutlet weak var dateTableViewCell: UITableViewCell!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var dateSelectTableViewCell: UITableViewCell!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var dateTimeTableViewCell: UITableViewCell!
+    @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var dateTimeSelectTableViewCell: UITableViewCell!
+    @IBOutlet weak var dateTimePicker: UIDatePicker!
     
     static let identifier = "EventEditViewController"
+    
+    let dateFormatter = DateFormatter()
     
     var event: HouseEvent!
     var onSave: ((HouseEvent) -> Void)?
@@ -30,6 +42,14 @@ class EventEditViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        let cell = tableView.cellForRow(at: indexPath)
+        if cell == dateTableViewCell {
+            dateSelectTableViewCell.isHidden = !dateSelectTableViewCell.isHidden
+        }
+        else if cell == dateTimeTableViewCell {
+            dateTimeSelectTableViewCell.isHidden = !dateTimeSelectTableViewCell.isHidden
+        }
     }
     
     private func alertEmptyTitle() {
@@ -42,13 +62,18 @@ class EventEditViewController: UITableViewController {
         present(alertController, animated: true, completion: nil)
     }
     
+    private func updateEvent() {
+        event.houseKey = RMUser.current!.houseKey
+        event.title = titleTextField.text!
+        //        event.descriptionText = descriptionTextField.text!
+    }
+    
     private func exit() {
         dismiss(animated: true, completion: nil)
     }
 
     @IBAction func save(_ sender: Any) {
-        event.title = titleTextField.text!
-        event.houseKey = RMUser.current!.houseKey
+        updateEvent()
         
         switch event.save() {
         case .success:
@@ -63,5 +88,23 @@ class EventEditViewController: UITableViewController {
     
     @IBAction func cancel(_ sender: Any) {
         exit()
+    }
+    
+    @IBAction func alldaySwitch_valueChanged(_ sender: UISwitch) {
+        let allDay = sender.isOn
+        dateTableViewCell.isHidden = !allDay
+        dateTimeTableViewCell.isHidden = allDay
+    }
+    
+    @IBAction func datePicker_valueChanged(_ sender: UIDatePicker) {
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        dateLabel.text = dateFormatter.string(from: sender.date)
+    }
+    
+    @IBAction func dateTimePicker_valueChanged(_ sender: UIDatePicker) {
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        dateTimeLabel.text = dateFormatter.string(from: sender.date)
     }
 }
